c5854f70fcc52bb5a5c46fce9401ed5b4970130f
Arpit Mishra <arpitmishra1547@gmail.com>
2026-03-09 17:41:46 +0530

    cleanser version of RBS

--- Recursion/src/Array/RBS.java
+++ Recursion/src/Array/RBS.java
@@ -3,7 +3,7 @@ package Array;
 public class RBS {
     public static void main(String[] args) {
         int[] arr = {5, 6, 7, 8, 9, 1, 2, 3};
-        System.out.println(search(arr, 4, 0, arr.length - 1));
+        System.out.println(search(arr, 9, 0, arr.length - 1));
     }
 
     static int search(int[] arr, int target, int s, int e) {
@@ -16,19 +16,22 @@ public class RBS {
             return m;
         }
 
-        if (arr[s] <= arr[m]) {
+        if (arr[s] <= arr[m]) { // left sorted
+
             if (target >= arr[s] && target <= arr[m]) {
-                return search(arr, target, s, m-1);
+                return search(arr, target, s, m - 1);
             } else {
-                return search(arr, target, m+1, e);
+                return search(arr, target, m + 1, e);
+            }
+
+        } else { // right sorted
+
+            if (target >= arr[m] && target <= arr[e]) {
+                return search(arr, target, m + 1, e);
+            } else {
+                return search(arr, target, s, m - 1);
             }
         }
-
-        if (target >= arr[m] && target <= arr[e]) {
-            return search(arr, target, m+1, e);
-        }
-
-        return search(arr, target, s, m-1);
     }
 
 }
