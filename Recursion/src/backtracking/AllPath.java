99961aed1483f7e2fb04648591cd73bf0cdc9e65
Arpit Mishra <arpitmishra1547@gmail.com>
2026-03-15 13:11:18 +0530

    knights solve

--- Recursion/src/backtracking/AllPath.java
+++ Recursion/src/backtracking/AllPath.java
@@ -1,5 +1,7 @@
 package backtracking;
 
+import java.util.Arrays;
+
 public class AllPath {
     public static void main(String[] args) {
         boolean[][] board = {
@@ -8,7 +10,7 @@ public class AllPath {
                 {true, true, true}
         };
         int[][] path = new int[board.length][board[0].length];
-        allPath("", board, 0, 0);
+        allPathPrint("", board, 0, 0, path ,1);
     }
 
 
@@ -44,6 +46,48 @@ public class AllPath {
         // this line is where the function will be over
         // so before the function gets removed, also remove the changes that were made by that function
         maze[r][c] = true;
+    }
+
+
+
+    static void allPathPrint(String p, boolean[][] maze, int r, int c, int[][] path, int step) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            path[r][c] = step;
+            for(int[] arr : path) {
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+
+        if (!maze[r][c]) {
+            return;
+        }
+
+        // i am considering this block in my path
+        maze[r][c] = false;
+        path[r][c] = step;
+        if (r < maze.length - 1) {
+            allPathPrint(p + 'D', maze, r+1, c, path, step+1);
+        }
+
+        if (c < maze[0].length - 1) {
+            allPathPrint(p + 'R', maze, r, c+1, path, step+1);
+        }
+
+        if (r > 0) {
+            allPathPrint(p + 'U', maze, r-1, c, path, step+1);
+        }
+
+        if (c > 0) {
+            allPathPrint(p + 'L', maze, r, c-1, path, step+1);
+        }
+
+        // this line is where the function will be over
+        // so before the function gets removed, also remove the changes that were made by that function
+        maze[r][c] = true;
+        path[r][c] = 0;
     }
 
 }
